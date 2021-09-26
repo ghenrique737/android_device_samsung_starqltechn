769dfa673fefc763c340aed6d6ef931b02096b7c
Gustavo <ghenrique737@gmail.com>
2021-09-26 14:16:46 -0300

    Include device blobs and change kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,4 +23,7 @@ DEVICE_PATH := device/samsung/starqltechn
 TARGET_OTA_ASSERT_DEVICE := starqltechn
 
 # Kernel
-TARGET_KERNEL_CONFIG := starqlte_chn_open_defconfig
+TARGET_KERNEL_CONFIG := lineage-starqltechn_defconfig
+
+# inherit from the proprietary version
+include vendor/samsung/starqltechn/BoardConfigVendor.mk
